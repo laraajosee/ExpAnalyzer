0457c5abce345c330ef0290785f71149b5282a5c
laraajosee <chemanuel94@gmail.com>
2022-02-13 23:22:03 -0600

    First Commint
    First commit "settings"

---
+++ src/proyectocupjlexwindows/ProyectoCupJlexWindows.java
@@ -0,0 +1,33 @@
+/*
+ * Ejemplo desarrollado por Erick Navarro
+ * Blog: e-navarro.blogspot.com
+ * Julio - 2018
+ */
+
+package proyectocupjlexwindows;
+
+import java.io.FileInputStream;
+
+/**
+ * Clase principal de la aplicación
+ * @author Erick
+ */
+public class ProyectoCupJlexWindows {
+
+    /**
+     * @param args argumentos de la linea de comando
+     */
+    public static void main(String[] args) {
+        
+        Interfaz n = new Interfaz();
+        n.show();
+        n.setLocationRelativeTo(null);
+    }
+    /**
+     * Método que interpreta el contenido del archivo que se encuentra en el path
+     * que recibe como parámentro
+     * @param path ruta del archivo a interpretar
+     */
+    
+    
+}
